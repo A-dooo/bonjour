ed9a2e444077b21f8f0a05a9e09f14790b5b252b
du1990 <du_1990@vip.sina.com>
2016-05-17 16:06:36 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -13,14 +13,31 @@ const store = configureSrore(rootReducer);
 
 
 
+
+
+const routerConfig=[
+	{
+		path:'/',
+		component:App,
+		indexRoute:{
+			component:First 
+		},
+		childRoutes:[
+      		{
+      			path:'about',
+      			component:About
+      		}
+    	]
+	},
+	{
+		path:"*",
+		component:Error
+	}
+
+]
+
 ReactDom.render((
   	<Provider store={store}>			
-		<Router  history={hashHistory}>
-			<Route  path="/" component={App}>  
-				<IndexRoute component={First}/>
-				<Route path="/about" component ={About} />
-			</Route>
-			<Route path="*"  component ={Error} />
-		</Router>
+		<Router  history={hashHistory} routes={routerConfig} />
 	</Provider>
 ), document.getElementById('container'))
